e3907f7babf65669c876638482832cb011cdc15d
Jai Khatri <f20190543@goa.bits-pilani.ac.in>
2022-08-07 23:29:28 +0530

    cursor trail

--- src/App.js
+++ src/App.js
@@ -5,11 +5,12 @@ import Skills from './components/Skills'
 import Projects from './components/Projects';
 import Contact from "./components/Contact"
 import Footer from "./components/Footer"
-
+import AnimatedCursor from "react-animated-cursor"
 
 function App() {
   return (
     <div className="App">
+        <AnimatedCursor innerSize={15} outerSize={5} innerScale={1}/>
       <NavbarTop/>
       <Banner/>
       <Skills/>
